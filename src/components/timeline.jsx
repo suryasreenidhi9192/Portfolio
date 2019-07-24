e6ad7be5b9ecedd8cd6c3f5ceded21d943a0f5f7
srinidhi bollapragada <suryasreenidhi9@gmail.com>
2019-07-24 13:12:49 -0400

    new files

---
+++ src/components/timeline.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+    render() {
+        return (
+            <div>
+                <section className="colorlib-experience" data-section="timeline">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                                <span className="heading-meta">highlights</span>
+                                <h2 className="colorlib-heading animate-box">Timeline</h2>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="timeline-centered">
+                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                                        <div className="timeline-entry-inner">
+                                            <div className="timeline-icon color-3">
+                                                <i className="icon-pen2" />
+                                            </div>
+                                            <div className="timeline-label">
+                                                <h2>Ph.D in Information Technology from University of Cumberlands<span>2019 to present</span></h2>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                                        <div className="timeline-entry-inner">
+                                            <div className="timeline-icon color-4">
+                                                <i className="icon-pen2" />
+                                            </div>
+                                            <div className="timeline-label">
+                                                <h2>Masters in Computer Science from Northwestern Polytechnic University<span>2014-2015</span></h2>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                                        <div className="timeline-entry-inner">
+                                            <div className="timeline-icon color-5">
+                                                <i className="icon-pen2" />
+                                            </div>
+                                            <div className="timeline-label">
+                                                <h2>Bachelors in Computer Science <span>2009-2013</span></h2>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                                        <div className="timeline-entry-inner">
+                                            <div className="timeline-icon color-none">
+                                            </div>
+                                        </div>
+                                    </article>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
